9b7a796babd5981da8ff4db5089b65ec91bdb1c0
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-06-01 17:41:07 +0800

    lesson1

---
+++ Lesson 1/myfirst_yoursecond.py
@@ -0,0 +1,34 @@
+# Selecting Substrings : Writing a Python Procedure
+
+# Let p and q each be strings containing two words separated by a space.
+
+# Examples:
+#    "bell hooks"
+#    "grace hopper"
+#    "alonzo church"
+
+# Write a procedure called myfirst_yoursecond(p,q) that returns True if the
+# first word in p equals the second word in q. Return False otherwise.
+
+def myfirst_yoursecond(p, q):
+    first = get_first(p)
+    second = get_second(q)
+    return first == second
+
+
+def get_first(w):
+    space_index = find_space(w)
+    return w[0:space_index]
+
+
+def get_second(w):
+    space_index = find_space(w)
+    return w[space_index + 1:]
+
+
+def find_space(w):
+    return w.find(" ")
+
+
+print(myfirst_yoursecond("bell hooks", "curer bell"))
+# >>> True
