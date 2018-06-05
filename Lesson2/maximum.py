a368c01bc17c7d99095a8701dba6c1c8d4ff87ec
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-06-05 09:43:02 +0800

    maximum

--- Lesson2/maximum.py
+++ Lesson2/maximum.py
@@ -11,8 +11,25 @@
 
 # Example:
 
-l = ['Barbara', 'kingsolver', 'wrote', 'The', 'Poisonwood', 'Bible']
-f = len(max(l, key=len))
-print(f)
+# l = ['Barbara', 'kingsolver', 'wrote', 'The', 'Poisonwood', 'Bible']
+# f = len(max(l, key=len))
 
+
+l = ['Barbara', 'kingsolver', 'wrote', 'The', 'Poisonwood', 'Bible']
+f = len
+
+
+def findmax(f, l):
+    best_ele_sofar = None
+    best_f_value_sofar = None
+    for i in range(len(l)):
+        elt = l[i]
+        f_value = f(elt)
+        if best_f_value_sofar is None or f_value > best_f_value_sofar:
+            best_ele_sofar = elt
+            best_f_value_sofar = f_value
+    return best_f_value_sofar
+
+
+print(findmax(f, l))
 # Try it on your own!
